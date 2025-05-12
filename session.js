b209175efc356c970f6944de2505e1d7cab70572
anjanaheroku4 <anjanabotheroku@gmail.com>
2025-05-12 13:01:17 +0530

    Update session.js

--- session.js
+++ session.js
@@ -40,7 +40,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || "ANJU-XPRO~MWVTmSQI#qnoUs2X5dDfcnIMWN4LoDle_f-X9OwDn2xHBRexSVww",
+    SESSION_ID: process.env.SESSION_ID || "ANJU-XPRO~QrVH2ShT#nThL_Dmjy8FWGVh5-PIwhy-SsmWx-fNxnR4BHel1JPE",
     GITHUB_TOKEN: process.env.GITHUB_TOKEN || "oPIMXcUfRIYbUdEj6hEYIbjVR2nzuH1y5Brj",
     GITHUB_USERNAME: process.env.GITHUB_USERNAME || "anjanaheroku4",
     SUDO: process.env.SUDO ? process.env.SUDO.split(",") : ["94717775628", "94758775628"],
